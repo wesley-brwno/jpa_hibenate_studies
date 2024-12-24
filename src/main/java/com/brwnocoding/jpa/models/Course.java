c04785ba0c6b26faa2fcec74b483b881f1cddd4c
Wesley Bruno <brunow@outlook.com>
2024-12-24 11:42:50 -0300

    Creates Entities

---
+++ src/main/java/com/brwnocoding/jpa/models/Course.java
@@ -0,0 +1,25 @@
+package com.brwnocoding.jpa.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private String description;
+}
---
+++ src/main/java/com/brwnocoding/jpa/models/Resources.java
@@ -0,0 +1,28 @@
+package com.brwnocoding.jpa.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Resources {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private int size;
+
+    private String url;
+}
