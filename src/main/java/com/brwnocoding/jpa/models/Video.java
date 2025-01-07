eb242cddc1c83d3c999d4b0b5556f08095a94b30
Wesley Bruno <wesleybruno910@gmail.com>
2025-01-06 21:09:23 -0300

    JPA inheritance, Polymorphism

--- src/main/java/com/brwnocoding/jpa/models/Video.java
+++ src/main/java/com/brwnocoding/jpa/models/Video.java
@@ -8,6 +8,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Polymorphism;
+import org.hibernate.annotations.PolymorphismType;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -17,6 +19,7 @@ import lombok.experimental.SuperBuilder;
 @Entity
 //@PrimaryKeyJoinColumn(name = "video_id")
 //@DiscriminatorValue("V")
+@Polymorphism(type = PolymorphismType.EXPLICIT)
 public class Video extends Resource {
 
     private int lenght;
