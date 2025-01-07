062122e440bdca6d4a740f5a2c5c27c979d77568
Wesley Bruno <wesleybruno910@gmail.com>
2025-01-06 21:21:28 -0300

    Embedded Id

---
+++ src/main/java/com/brwnocoding/jpa/models/embeded/Order.java
@@ -0,0 +1,23 @@
+package com.brwnocoding.jpa.models.embeded;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_order")
+public class Order {
+
+    @EmbeddedId
+    private OrderId id;
+
+    private String orderInfo;
+
+    private String anotherField;
+}
---
+++ src/main/java/com/brwnocoding/jpa/models/embeded/OrderId.java
@@ -0,0 +1,21 @@
+package com.brwnocoding.jpa.models.embeded;
+
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class OrderId implements Serializable {
+
+    private String username;
+    private LocalDateTime orderDate;
+
+}
